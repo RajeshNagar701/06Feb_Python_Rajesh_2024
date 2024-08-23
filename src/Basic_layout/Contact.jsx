4b732094bf118cb71c88615f58a59951c86db50d
GauravGoswami12967 <gaurav12967@gmail.com>
2024-08-23 11:57:07 +0530

    first

---
+++ src/Basic_layout/Contact.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Header from './Common/Header'
+import Navbar from './Common/Navbar'
+import Sidebar from './Common/Sidebar'
+import Footer from './Common/Footer'
+
+function Contact() {
+    return (
+        <div>
+            <Header />
+            <Navbar />
+            <div class="container mt-5">
+                <div class="row">
+                    <Sidebar />
+                    <div class="col-sm-8">
+                        <h2>Contact Form</h2>
+                        <form action="/action_page.php">
+                            <div class="mb-3 mt-3">
+                                <label for="email" class="form-label">Email:</label>
+                                <input type="email" class="form-control" id="email" placeholder="Enter email" name="email"/>
+                            </div>
+                            <div class="mb-3">
+                                <label for="pwd" class="form-label">Password:</label>
+                                <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd"/>
+                            </div>
+                            <div class="form-check mb-3">
+                                <label class="form-check-label">
+                                    <input class="form-check-input" type="checkbox" name="remember"/> Remember me
+                                </label>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Contact
---
+++ src/Routing/Pages/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from '../Common/Header'
+import Navbar from '../Common/Navbar'
+import Sidebar from '../Common/Sidebar'
+import Footer from '../Common/Footer'
+
+function About() {
+    return (
+        <div>
+            <Header />
+            <Navbar />
+            <div class="container mt-5">
+                <div class="row">
+                    <Sidebar />
+                    <div class="col-sm-8">
+                        <h2>About Page</h2>
+                     
+
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default About
